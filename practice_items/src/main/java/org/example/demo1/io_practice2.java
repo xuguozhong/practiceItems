cf2039ef4135d1e04bc6b8c36a07d0c2215584df
xuguozhong <1342979708@qq.com>
2024-04-23 16:23:23 +0800

    第二次提交

--- practice_items/src/main/java/org/example/demo1/io_practice2.java
+++ practice_items/src/main/java/org/example/demo1/io_practice2.java
@@ -5,7 +5,6 @@ public class io_practice2 {
     public static void main(String[] args) {
         System.out.println("asda");
         System.out.println("asda");
-        System.out.println("asda");
     }
 
 
