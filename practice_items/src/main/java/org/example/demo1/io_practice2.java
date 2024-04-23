d5eb7cca09080a0245d8d47d69509b82f75e8169
xuguozhong <1342979708@qq.com>
2024-04-23 16:18:17 +0800

    第二次提交

--- practice_items/src/main/java/org/example/demo1/io_practice2.java
+++ practice_items/src/main/java/org/example/demo1/io_practice2.java
@@ -5,6 +5,7 @@ public class io_practice2 {
     public static void main(String[] args) {
         System.out.println("asda");
         System.out.println("asda");
+        System.out.println("asda");
     }
 
 
